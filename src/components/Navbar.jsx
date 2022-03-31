ed2af0f228d455e9014f290d5b718b982d8aeaee
AtaDemirkiran <49242945+AtaDemirkiran@users.noreply.github.com>
2022-04-01 00:18:07 +0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,33 +2,35 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 const Navbar = () => {
 
-    const [color, setColor] = useState('#18142f')
+    // const [color, setColor] = useState('#18142f')
 
     return (
-        <nav className="navbar navbar-expand-lg white" style={{ backgroundColor: '#ff4a59' }}>
+        <nav className="navbar navbar-expand-lg white">
 
             <div className="container">
 
-                <h4 className='h4 pt-2' style={{ color }}>VALORANT APP</h4>
+                <Link to='/' className='navbarItem'>
+                    <h4 className='h4 pt-2 '>VALORANT APP</h4>
+                </Link>
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
-                    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation" style={{ backgroundColor: color }}>
+                    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation" >
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse ml-5" id="basicExampleNav">
                     <ul className="navbar-nav mr-auto font-weight-bold">
                         <li className="nav-item ml-3 mt-1">
-                            <Link style={{ color }} to={'/'}>Home</Link>
+                            <Link className='navbarItem' to={'/'}>Home</Link>
                         </li>
                         <li className="nav-item ml-3 mt-1">
-                            <Link to={'/agents'} style={{ color }}>Agents</Link>
+                            <Link to={'/agents'} className='navbarItem'>Agents</Link>
                         </li>
                         <li className="nav-item ml-3 mt-1">
-                            <Link to='/weapons' style={{ color }}>Weapons</Link>
+                            <Link to='/weapons' className='navbarItem'>Weapons</Link>
                         </li>
                         <li className='nav-item ml-3 mt-1'>
-                            <Link to='/maps' style={{ color }}>Maps</Link>
+                            <Link to='/maps' className='navbarItem'>Maps</Link>
                         </li>
                     </ul>
                 </div>
